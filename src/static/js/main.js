ae5f67513b934cf9398fda4b344eba2947a2bcab
Victor <thevictorxx@gmail.com>
2022-02-04 22:39:16 -0300

    ADD: Dropdown Menu

--- src/static/js/main.js
+++ src/static/js/main.js
@@ -8,3 +8,26 @@ if ($cerrarSessionNav) {
     window.location.href = "/";
   });
 }
+
+/*  CERRAR ALERTAS */
+/* estilos.view.js */
+(() => {
+  const $$closeAlert = document.querySelectorAll(".alert>.close");
+  $$closeAlert.forEach(($closeAlert) => {
+    $closeAlert.addEventListener("click", (e) => {
+      e.preventDefault();
+      $closeAlert.parentElement.classList.add("d-none");
+    });
+  });
+})();
+
+// DROPDOWN MENU
+const $$dropdown = document.querySelectorAll(".dropdown");
+$$dropdown.forEach(($dropdown) => {
+  console.log($dropdown.firstChild.nextSibling);
+  $dropdown.firstChild.nextSibling.addEventListener("click", function (e) {
+    e.preventDefault();
+    this.classList.toggle("active");
+    $dropdown.querySelector(".dropdown-content").classList.toggle("show");
+  });
+});
