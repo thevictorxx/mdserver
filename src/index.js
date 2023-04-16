9967d0179ade11886fd961048227e094b0d7f7ea
Victor <thevictorxx@gmail.com>
2023-04-16 17:54:22 -0300

    ADD: Se agregan nuevos console log para la consola

--- src/index.js
+++ src/index.js
@@ -2,6 +2,13 @@ require("dotenv").config();
 const app = require("./app");
 
 app.listen(process.env.PORT, () => {
-  console.log(`Corriendo en puerto ${process.env.PORT}`);
+  console.log("████████████████████████████████████")
+  console.log("███                              ███")
+  console.log("███           MD Server          ███")
+  console.log("███                              ███")
+  console.log("████████████████████████████████████")
+  console.log(`Puerto: ${process.env.PORT}`);
+  console.log(`Access Token Expires: ${process.env.ACCESS_TOKEN_EXPIRES_IN}`);
   console.log(`http://localhost:${process.env.PORT}`);
+  console.log(`${process.env.HOST}`);
 });
